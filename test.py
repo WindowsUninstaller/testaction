4bc4a86c19f2024a907cbf675f4dcde0cd823c7c
Windows卸载程序 <win_uninstaller_2024@hotmail.com>
2026-03-14 11:29:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import time
 print('Hello, Actions!')
+writetext=f'Hello, Actions!\ntime:{time.time()}\nperf_counter:{time.perf_counter()}\n'
+print(writetext)
 with open('output.bin', 'w') as file:
-    print('Hello, Actions!', file=file)
-    print(f'time:{time.time()}', file=file)
-    print(f'perf_counter:{time.perf_counter()}', file=file)
+    print(writetext, file=file)
